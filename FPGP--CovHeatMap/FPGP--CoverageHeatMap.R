7bd4590a60de0e46b1063103dbabddd285c53fc9
George Pacheco <37849281+layka-pacheco@users.noreply.github.com>
2021-05-31 01:56:18 +0200

    Small changes.

--- FPGP--CovHeatMap/FPGP--CoverageHeatMap.R
+++ FPGP--CovHeatMap/FPGP--CoverageHeatMap.R
@@ -1,11 +1,17 @@
-setwd("C:/Users/ganpa/OneDrive - Danmarks Tekniske Universitet/Skrivebord/DTU-Aqua--Postdoc/Past/Core/FPGP -- FINISHING/FPGP--GitHub/FeralPigeonGenomicsProject/FPGP--CovHeatMap/")
+### The BEGINNING ~~~~
+##
+# Creates FPGP--CoverageHeatMap | By George PACHECO
 
+# Sets Working Directory:
 
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
-###   Creates FPGP--Map | By George PACHECO   ###
-
-
+# Loads libraries:
 
 data=read.table("Loci_Merged.coverage.km.tsv", , sep="\t", check.names=FALSE)
 library(pheatmap)  
 pheatmap(as.matrix(data), border_color="black", treeheight_row=125, treeheight_col=125, cellwidth=10, cellheight=10, cutree_rows=12, cutree_cols=10, filename = "PBGP Loci Stats Loci_Merged.coverage -- To be Edited.pdf")
+
+#
+##
+### The END ~~~~
