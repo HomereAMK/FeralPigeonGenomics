4998d766ad8f1b35e53c30ab310d9d7e5df8bf2d
George Pacheco <37849281+layka-pacheco@users.noreply.github.com>
2021-06-02 16:28:00 +0200

    Minor.

--- FPGP--PopGenEstimates/FPGP--PopGenEstimates.R
+++ FPGP--PopGenEstimates/FPGP--PopGenEstimates.R
@@ -28,18 +28,19 @@ Data$Population <- factor(Data$Population, ordered = T, levels = c("Cpalumbus", 
 
 # Creates the plots:
 
-ggplot(Data, aes(factor(Population), tW)) + geom_point(fill = "#F79999", size= 2) +
-     labs(x = Data$Population, y = "Watson's Theta") +
-     theme(panel.background = element_rect(fill = '#FAFAFA'),
-           panel.grid.minor = element_blank(),
-           panel.border = element_blank(),
-           axis.line = element_line(colour = "#000000", size = 0.3),
-           axis.title.x = element_blank(),
-           axis.title.y = element_text(size = 22, face = "bold", color = "#000000", margin = margin(t = 0, r = 20, b = 0, l = 0)),
-           axis.text.x = element_text(colour="#000000", size = 16, angle = 90, vjust = 0.5, hjust = 1),
-           axis.text.y = element_text(color="#000000", size = 16),
-           axis.ticks.x = element_blank(),
-           axis.ticks.y = element_line(color="#000000", size=0.3),
-           legend.position = "none")
+ggplot(Data, aes(factor(Population), tW)) +
+       geom_point(fill = "#F79999", size= 2) +
+        labs(x = Data$Population, y = "Watson's Theta") +
+        theme(panel.background = element_rect(fill = '#FAFAFA'),
+              panel.grid.minor = element_blank(),
+              panel.border = element_blank(),
+              axis.line = element_line(colour = "#000000", size = 0.3),
+              axis.title.x = element_blank(),
+              axis.title.y = element_text(size = 22, face = "bold", color = "#000000", margin = margin(t = 0, r = 20, b = 0, l = 0)),
+              axis.text.x = element_text(colour="#000000", size = 16, angle = 90, vjust = 0.5, hjust = 1),
+              axis.text.y = element_text(color="#000000", size = 16),
+              axis.ticks.x = element_blank(),
+              axis.ticks.y = element_line(color="#000000", size=0.3),
+              legend.position = "none")
 
 ggsave(file = "PopGenStats-tW.eps", device = cairo_pdf, height = 3.6, width = 6, scale = 2, dpi = 1000)
