460dfbd249392a393792aacdb3c6adb2cc77b86d
George Pacheco <37849281+layka-pacheco@users.noreply.github.com>
2021-06-04 15:24:08 +0200

    Minor.

--- FPGP--Analyses/FPGP--PopGenEstimates/FPGP--PopGenEstimates.R
+++ FPGP--Analyses/FPGP--PopGenEstimates/FPGP--PopGenEstimates.R
@@ -13,54 +13,54 @@ pacman::p_load(ggplot2, scales, extrafont, dplyr, grid, lubridate, cowplot, egg,
 
 # Imports extra fonts:
 
-loadfonts(device = 'win', quiet = TRUE)
+loadfonts(device = "win", quiet = TRUE)
 
 # Loads the data
 
-Data <- read.table('GoodSamples_NoSrisoriaNoCpalumbus-DoSaf-WithWrapper-DoThetas-NoWrapper.PopGenSummary', sep = '\t', header = FALSE)
+Data <- read.table("MDS.PopGenSummary", sep = "\t", header = FALSE)
 
 # Adds column names:
 
-colnames(Data) <- c('Population', 'NSites', 'Nucleotide Diversity', 'Watson's Theta', 'Tajima's D', 'BioStatus')
+colnames(Data) <- c("Population", "NSites", "Nucleotide Diversity", "Watson's Theta", "Tajima's D", "BioStatus")
 
 # Reorganises the data:
 
 Data$Population <- factor(Data$Population, ordered = T,
-                   levels = c('Torshavn', 'Crete', 'Sardinia', 'Vernelle', 'Wadi Hidan', 'Pigeon Island', 'Trincomalee',
-                              'Lisbon', 'Guimaraes', 'Barcelona', 'London', 'Berlin', 'Copenhagen', 'Prague', 'Tel Aviv', 'Abadeh', 'Tehran', 'Lahijan', 'Nowshahr', 'Isfahan', 'Colombo',
-                              'Denver', 'Salt Lake City', 'Tlaxcala de Xicohtencatl', 'Mexico City', 'Monterrey',
-                              'San Cristobal de las Casas', 'Santiago', 'Salvador', 'Tatui', 'Johannesburg', 'Nairobi', 'Perth',
-                              'Tel Aviv Colony'))
+                   levels = c("Torshavn", "Crete", "Sardinia", "Vernelle", "Wadi Hidan", "Pigeon Island", "Trincomalee",
+                              "Lisbon", "Guimaraes", "Barcelona", "London", "Berlin", "Copenhagen", "Prague", "Tel Aviv", "Abadeh", "Tehran", "Lahijan", "Nowshahr", "Isfahan", "Colombo",
+                              "Denver", "Salt Lake City", "Tlaxcala de Xicohtencatl", "Mexico City", "Monterrey",
+                              "San Cristobal de las Casas", "Santiago", "Salvador", "Tatui", "Johannesburg", "Nairobi", "Perth",
+                              "Tel Aviv Colony"))
 
 # Prepares the data:
 
-Data_lg <- gather(Data, Estimate, Value, 'Nucleotide Diversity', 'Watson's Theta', 'Tajima's D')
+Data_lg <- gather(Data, "Estimate", "Value", Nucleotide_Diversity, Watson's Theta, Tajima's D)
 
 # Creates the plots:
 
 ggplot(data = Data_lg, aes(x = get(Population))) +
-  geom_point(aes(x = Population, y = Value, fill = BioStatus), colour = 'black', shape = 21, size = 3.5, alpha = .9) +
-  facet_grid(vars(Estimate), scales = 'free') +
-  scale_fill_manual(values = c('#56B4E9', '#E69F00', '#44AA99', '#F0E442'), drop = FALSE) +
-  scale_colour_manual(values = c('#56B4E9', '#E69F00', '#44AA99', '#F0E442'), drop = FALSE) +
-  theme(panel.background = element_rect(fill = '#FAFAFA'),
-        panel.grid.major.x = element_line(color = '#ededed', linetype = 'dashed', size = .00005),
+  geom_point(aes(x = Population, y = Value, fill = BioStatus), colour = "black", shape = 21, size = 3.5, alpha = .9) +
+  facet_grid(vars(Estimate), scales = "free") +
+  scale_fill_manual(values = c("#56B4E9", "#E69F00", "#44AA99", "#F0E442"), drop = FALSE) +
+  scale_colour_manual(values = c("#56B4E9", "#E69F00", "#44AA99", "#F0E442"), drop = FALSE) +
+  theme(panel.background = element_rect(fill = "#FAFAFA"),
+        panel.grid.major.x = element_line(color = "#ededed", linetype = "dashed", size = .00005),
         panel.grid.major.y = element_blank(),
         panel.grid.minor = element_blank(), 
         panel.border = element_blank(),
-        axis.line = element_line(colour = '#000000', size = .3),
+        axis.line = element_line(colour = "#000000", size = .3),
         axis.title = element_blank(),
-        axis.text.x = element_text(colour='#000000', size = 16, face = 'bold', family = 'Helvetica', angle = 90, vjust = .5, hjust = 1),
-        axis.text.y = element_text(color='#000000', size = 16, family = 'Helvetica'),
-        axis.ticks.x = element_line(color='#000000', size = .3),
-        axis.ticks.y = element_line(color='#000000', size = .3),
-        strip.background = element_rect(colour = '#000000', fill = '#d6d6d6', size = .05),
-        strip.text = element_text(colour='#000000', size = 14, face = 'bold', family = 'Georgia'),
-        legend.position = 'none')
+        axis.text.x = element_text(colour="#000000", size = 16, face = "bold", family = "Helvetica", angle = 90, vjust = .5, hjust = 1),
+        axis.text.y = element_text(color="#000000", size = 16, family = "Helvetica"),
+        axis.ticks.x = element_line(color="#000000", size = .3),
+        axis.ticks.y = element_line(color="#000000", size = .3),
+        strip.background = element_rect(colour = "#000000", fill = "#d6d6d6", size = .05),
+        strip.text = element_text(colour="#000000", size = 14, face = "bold", family = "Georgia"),
+        legend.position = "right")
 
 # Saves the final plot:
 
-ggsave(file = 'FPGP--PopGenEstimates.pdf', device = cairo_pdf, height = 8, width = 12, scale = 1.5, dpi = 1000)
+ggsave(file = "FPGP--PopGenEstimates.pdf", device = cairo_pdf, height = 8, width = 12, scale = 1.5, dpi = 1000)
 
 #
 ##
