4cda039758f34d7692d15cec5a05f2f6fedbe0ea
George Pacheco <37849281+layka-pacheco@users.noreply.github.com>
2021-06-08 14:02:27 +0200

    Improved. Better organisation.

--- FPGP--Analyses/FPGP--PopGenEstimates/FPGP--PopGenEstimates.R
+++ FPGP--Analyses/FPGP--PopGenEstimates/FPGP--PopGenEstimates.R
@@ -1,6 +1,6 @@
 ### The BEGINNING ~~~~
 ##
-# ~ Plots FPGP--PopGenEstimates | By George PACHECO
+# ~ Plots FPGP--PopGenEstimates | By Marie-Christine RUFENER & George PACHECO
 
 
 # Sets working directory ~
@@ -20,153 +20,149 @@ PopGen <- read.table("FPGP--MDS.PopGenSummary", sep = "\t", header = FALSE); hea
 Hets <- read.table("FPGP--HetProportions.HetSummary", sep = "\t", header = FALSE); head(Hets)
 
 
-# Tidies up data frames ~
+# Adds column names ~
 colnames(PopGen) <- c("Population", "NSites", "Nucleotide_Diversity", "Watson_Theta", "Tajima_D", "BioStatus")
 colnames(Hets) <- c("Sample_ID", "Population", "Het", "DataType")
 
-levels(PopGen$Population)
-PopGen$Population <- as.factor(gsub(" ", "", PopGen$Population, fixed = TRUE))
-PopGen$BioStatus <- as.factor(gsub(" ", "_", PopGen$BioStatus)) #Now do the reverse thing with BioStatus
 
-## Adds new column to Hets df (BioStatus):
-# See suggestion below for a shorter solution? 
-# Note: Keep consistency in colum names that are common across the two DFs (eg. BioStatus vs. BiologicalStatus)
+# Tidies up data frames ~
+levels(PopGen$Population)
+PopGen$Population <- as.factor(gsub(" ", "", PopGen$Population))
+PopGen$BioStatus <- as.factor(gsub(" ", "_", PopGen$BioStatus))
+
+
+## Expands Hets by adding BioStatus ~
 Hets$BioStatus <- ifelse(Hets$Population %in% c("Torshavn","Ejde","Sumba","LjosAir","Kunoy","Nolsoy","Crete",
                                                 "Sardinia","Vernelle","WadiHidan","PigeonIsland","Trincomalee"), "Remote_Localities_Within_Natural_Range",
-                  ifelse(Hets$Population %in% c("Guimaraes","Lisbon","Barcelona","Berlin","Cambridge",
-                                                "Colombo","Copenhagen","London","Prague","Jihlava","Abadeh",
-                                                "Isfahan","Lahijan","Nowshahr","Tehran","TelAviv"), "Urban_Localities_Within_Natural_Range",
-                  ifelse(Hets$Population %in% c("SaltLakeCity","Denver","Virginia","TlaxcalaDeXicohtencatl",
-                                                "MexicoCity","Monterrey","SanCristobalDeLasCasas","Santiago",
-                                                "Salvador","Tatui","Johannesburg","Nairobi","Perth"), "Localities_Outside_Natural_Range",
-                  ifelse(Hets$Population %in% c("TelAvivColony","Wattala", "Wellawatte"), "Captive_Populations", "Outgroups"))))
-                 
+                         ifelse(Hets$Population %in% c("Guimaraes","Lisbon","Barcelona","Berlin","Cambridge",
+                                                       "Colombo","Copenhagen","London","Prague","Jihlava","Abadeh",
+                                                       "Isfahan","Lahijan","Nowshahr","Tehran","TelAviv"), "Urban_Localities_Within_Natural_Range",
+                                ifelse(Hets$Population %in% c("SaltLakeCity","Denver","Virginia","TlaxcalaDeXicohtencatl",
+                                                              "MexicoCity","Monterrey","SanCristobalDeLasCasas","Santiago",
+                                                              "Salvador","Tatui","Johannesburg","Nairobi","Perth"), "Localities_Outside_Natural_Range",
+                                       ifelse(Hets$Population %in% c("TelAvivColony","Wattala", "Wellawatte"), "Captive_Populations", "Outgroups"))))
+
+
+# NOT SURE WHAT TO SAY HERE ~                  
 Hets$BioStatus <- as.factor(Hets$BioStatus)
 
-HetsUp <- Hets #To keep consistency with your script
 
-# Removes unwanted populations:
+# Gives the expanded Hets another name ~ 
+HetsUp <- Hets
+
+
+# Removes unwanted populations ~
 UnwantedPops <- c("Virginia", "Ejde", "Sumba", "LjosAir", "Kunoy", "Nolsoy", "Cambridge", "Jihlava",
                   "Wattala", "Wellawatte", "Srisoria", "Cpalumbus", "Crupestris")
 HetsUp <- filter(HetsUp, !Population %in% UnwantedPops)
-HetsUp$Population <- factor(HetsUp$Population) #Drop empty factor levels
-
-## Standardize column names and/or factor levels that are common across the DFs
-# This is extremely important if later below you will have to merge the two datasets for your potting routine
-
-setdiff(colnames(PopGen), colnames(HetsUp)) #Population & BioStatus seems ok
 
 
-# Population
-nlevels(PopGen$Population); nlevels(HetsUp$Population) #FIXME: There seems to be a missing Population in your GenPops DF. Is this normal? Let's check below which Pop. is missing
-
-#Check missing Population
-setdiff(levels(HetsUp$Population), levels(PopGen$Population))
-setdiff(levels(PopGen$Population),levels(HetsUp$Population))
-# "Virginia" is the missing population; besides, note that "SanCristobalDeLasCasas" & "TlaxcalaDeXicohtencatl" are spelled differentely. Let's fix this:
+# NOT SURE WHAT TO SAY HERE ~
+HetsUp$Population <- factor(HetsUp$Population)
 
 
-levels(PopGen$Population)[c(23, 30)] <- c(
-                                       "SanCristobalDeLasCasas",
-                                       "TlaxcalaDeXicohtencatl")
+# Checkpoint I ~
+#setdiff(colnames(PopGen), colnames(HetsUp))
+#nlevels(PopGen$Population); nlevels(HetsUp$Population)
+#setdiff(levels(HetsUp$Population), levels(PopGen$Population))
+#setdiff(levels(PopGen$Population), levels(HetsUp$Population))
+
+# Corrects population names in Hets ~ ¡It is very disturbing that this needs to be done here -- please consider modifying the original .sh script!
+levels(PopGen$Population)[c(23, 30)] <- c("SanCristobalDeLasCasas",
+                                          "TlaxcalaDeXicohtencatl")
 
 
-# BioStatus 
-nlevels(PopGen$BioStatus); nlevels(HetsUp$BioStatus) #FIXME: Same as for "Population"
+# Checkpoint II ~ 
+#nlevels(PopGen$BioStatus); nlevels(HetsUp$BioStatus)
+#setdiff(levels(HetsUp$BioStatus), levels(PopGen$BioStatus))
 
-#Check missing BioStatus
-setdiff(levels(HetsUp$BioStatus), levels(PopGen$BioStatus))
-# "Outgroups" is the missing BioStatus
-
-
-## Reorganises the data:
-PopGen$Population <- factor(PopGen$Population, ordered = T,
-                            levels = c("Torshavn", "Crete", "Sardinia", "Vernelle", "WadiHidan", "PigeonIsland", "Trincomalee",
-                                       "Lisbon", "Guimaraes", "Barcelona", "London", "Berlin", "Copenhagen", "Prague", "TelAviv", 
-                                       "Abadeh", "Tehran", "Lahijan", "Nowshahr", "Isfahan", "Colombo", "Denver", "SaltLakeCity", 
-                                       "TlaxcalaDeXicohtencatl", "MexicoCity", "Monterrey","SanCristobalDeLasCasas", "Santiago", 
-                                       "Salvador", "Tatui", "Johannesburg", "Nairobi", "Perth", "TelAvivColony"))
-
-HetsUp$Population <- factor(HetsUp$Population, ordered = T,
-                            levels = c("Torshavn", "Crete", "Sardinia", "Vernelle", "WadiHidan", "PigeonIsland", "Trincomalee",
-                                       "Lisbon", "Guimaraes", "Barcelona", "London", "Berlin", "Copenhagen", "Prague", "TelAviv", "Abadeh", "Tehran", "Lahijan", "Nowshahr", "Isfahan", "Colombo",
-                                       "Denver", "SaltLakeCity", "Virginia", "TlaxcalaDeXicohtencatl", "MexicoCity", "Monterrey", "SanCristobalDeLasCasas", "Santiago", "Salvador", "Tatui",
-                                       "Johannesburg", "Nairobi", "Perth",
-                                       "TelAvivColony"))
 
 # Converts DF from wide into long ~
 PopGenUp <- gather(PopGen, Estimate, Value, "Nucleotide_Diversity", "Watson_Theta", "Tajima_D")
 
 
-# Includes data ID column ~
+# Adds data ID column to each DF ~
 PopGenUp$ID <- factor(paste("PopGen"))
 HetsUp$ID <- factor(paste("Hets"))
 
-setdiff(colnames(PopGenUp),colnames(HetsUp))
-setdiff(colnames(HetsUp),colnames(PopGenUp))
 
+# Checkpoint III ~
+#setdiff(colnames(PopGenUp),colnames(HetsUp))
+#setdiff(colnames(HetsUp),colnames(PopGenUp))
+
+
+# NOT SURE WHAT TO SAY HERE ~
 PopGenUp$Sample_ID <- NA
 PopGenUp$Het <- NA
 PopGenUp$DataType <- NA
-
-
 HetsUp$NSites <- NA
 HetsUp$Estimate <- NA
 HetsUp$Value <- NA
 
 
-## Reorders columns ~
+# Intersects the 2 DFs ~
 intersect(colnames(PopGenUp), colnames(HetsUp))
 
-PopGenUp <- PopGenUp[,c("Population","NSites","BioStatus","Estimate","Value","ID","Sample_ID","Het","DataType")]
-HetsUp <- HetsUp[,c("Population","NSites","BioStatus","Estimate","Value","ID","Sample_ID","Het","DataType")]
+
+# Reorders columns within each DF ~
+PopGenUp <- PopGenUp[,c("Population", "NSites", "BioStatus", "Estimate", "Value","ID", "Sample_ID", "Het", "DataType")]
+HetsUp <- HetsUp[,c("Population", "NSites", "BioStatus", "Estimate", "Value", "ID", "Sample_ID", "Het", "DataType")]
 
 
-## Bind the 2 DFs based on common columns (Population & BioStatus) ~
+# Bind the 2 DFs based on common columns (Population | BioStatus) ~
 fulldf <- rbind(PopGenUp, HetsUp)
 
-## Includes label for empty factor level (related to PHS)
+
+# Includes label for empty factor level (related to PHS) ~
 idx <- which(fulldf$ID == "Hets")
 fulldf[idx,"Estimate"] <- rep("PHS", length(idx))
 fulldf$Estimate <- factor(fulldf$Estimate) #Set to factor for plotting
 
 
+# Reorders facet ~
 fulldf$Estimate <-
- factor(fulldf$Estimate, ordered = T, levels = c(
-                                               "PHS",
-                                               "Nucleotide_Diversity",
-                                               "Tajima_D",
-                                               "Watson_Theta"))
+  factor(fulldf$Estimate, ordered = T, levels = c("PHS",
+                                                  "Nucleotide_Diversity",
+                                                  "Tajima_D",
+                                                  "Watson_Theta"))
 
 # Corrects the facet labels ~
+ylable <- c("Nucleotide_Diversity" = "Nucelotide Diversity",
+            "Tajima_D"= "Tajima's D",
+            "Watson_Theta" = "Watson's Theta",
+            "PHS"= "Heterozygosity")
 
-ylable <- c(
-          "Nucleotide_Diversity" = "Nucelotide Diversity",
-          "Tajima_D"= "Tajima's D",
-          "Watson_Theta" = "Watson's Theta",
-          "PHS"= "Heterozygosity")
 
-# Corrects the facet labels ~
+# Corrects population names ~
+levels(fulldf$Population <- sub("Torshavn", "Tórshavn", fulldf$Population))
+levels(fulldf$Population <- sub("WadiHidan", "Wadi Hidan", fulldf$Population))
+levels(fulldf$Population <- sub("Tatui", "Tatuí", fulldf$Population))
+levels(fulldf$Population <- sub("PigeonIsland", "Pigeon Island", fulldf$Population))
+levels(fulldf$Population <- sub("Guimaraes", "Guimarães", fulldf$Population))
+levels(fulldf$Population <- sub("SaltLakeCity", "Salt Lake City", fulldf$Population))
+levels(fulldf$Population <- sub("MexicoCity", "Mexico City", fulldf$Population))
+levels(fulldf$Population <- sub("TlaxcalaDeXicohtencatl", "Tlaxcala de Xicohténcatl", fulldf$Population))
+levels(fulldf$Population <- sub("SanCristobalDeLasCasas", "San Cristóbal de las Casas", fulldf$Population))
+levels(fulldf$Population <- sub("TelAvivColony", "Tel Aviv Colony", fulldf$Population))
+levels(fulldf$Population <- sub("TelAviv", "Tel Aviv", fulldf$Population))
 
-levels(fulldf$Population)[c(1, 5, 6, 9, 23, 24, 25, 27, 34)] <- c(
-                                                                 "Tórshavn",
-                                                                 "Wadi Hidan",
-                                                                 "Pigeon Island",
-                                                                 "Guimarães",
-                                                                 "Salt Lake City",
-                                                                 "Tlaxcala de Xicohtencatl",
-                                                                 "Mexico City",
-                                                                 "San Cristobal de las Casas",
-                                                                 "TelAvivColony")
+
+# Reorders populations ~
+fulldf$Population <- factor(fulldf$Population, ordered = T,
+                            levels = c("Tórshavn", "Crete", "Sardinia", "Vernelle", "Wadi Hidan", "Pigeon Island", "Trincomalee",
+                                       "Lisbon", "Guimarães", "Barcelona", "London", "Berlin", "Copenhagen", "Prague", "Tel Aviv", 
+                                       "Abadeh", "Tehran", "Lahijan", "Nowshahr", "Isfahan", "Colombo", "Denver", "Salt Lake City", 
+                                       "Tlaxcala de Xicohténcatl", "Mexico City", "Monterrey","San Cristóbal de las Casas", "Santiago", 
+                                       "Salvador", "Tatuí", "Johannesburg", "Nairobi", "Perth", "Tel Aviv Colony"))
+
 
 # Creates the panel ~
-
 PopGennEstimates <- 
-ggplot() + 
+  ggplot() + 
   geom_point(data = subset(fulldf, ID =="PopGen"),
              aes(x = Population, y = Value, fill = BioStatus), colour = "black", shape = 21, size = 3.5, alpha = .9) +
   geom_boxplot(data = subset(fulldf, ID == "Hets"),
-             aes(x = Population, y = Het, fill = BioStatus), outlier.size = 1.5, width = 0.3) +
+               aes(x = Population, y = Het, fill = BioStatus), outlier.size = 1.5, width = 0.3) +
   facet_grid(Estimate ~. , scales = "free", labeller = labeller(Estimate = ylable)) +
   scale_fill_manual(values = c("#56B4E9", "#E69F00", "#44AA99", "#F0E442"),
                     labels = gsub("_", " ", fulldf$BioStatus)) +
