1fde057c497c127a1bb0d1558a497ead26bc5618
George Pacheco <37849281+layka-pacheco@users.noreply.github.com>
2021-06-10 19:53:57 +0200

    FPG--CovHeatMap improved.

--- FPG--Plots/FPG--Stats/FPG--CovHeatMap/FPGP--CoverageHeatMap.R
+++ FPG--Plots/FPG--Stats/FPG--CovHeatMap/FPGP--CoverageHeatMap.R
@@ -3,6 +3,10 @@
 # ~ Creates FPGP--CoverageHeatMap | By George PACHECO
 
 
+# Cleans the environment ~ 
+rm(list=ls())
+
+
 # Sets working directory ~
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
@@ -29,21 +33,25 @@ Dist_Weighted <- dist(t(KMs_Weighted), method = "manhattan")
 NC <- ncol(KMs)
 NR <- nrow(KMs)
 
+# Defines QC ~ 
+QC <- data.frame(matrix(ncol = 1, nrow = 488, dimnames = list(NULL, c("SampleStatus"))))
+rownames(QC) <- colnames(KMs)
+QC$SampleStatus <- ifelse(rownames(QC) %in% c("FPGP_1-Blank-GBS", "FPGP_2-Blank-GBS", "FPGP_3-Blank-GBS", "FPGP_4-Blank-GBS",
+                                                        "FPGP_5-Blank-GBS", "FPGP_Extra-Blank-GBS", "Sumba_02-GBS", "Nolsoy_02-GBS",
+                                                        "Salvador_08-GBS", "Sumba_03-GBS", "Nolsoy_01-GBS", "Nolsoy_04-GBS", "London_13-GBS",
+                                                        "Salvador_12-GBS", "Berlin_16-GBS", "Salvador_11-GBS"), "BadSample", "GoodSample")
 
-# Creates the plot:
-Oi <- pheatmap(KMs,
-         border_color="black",
+# Defines QC colours ~
+colours = list(SampleStatus = c(BadSample = "#bf00ff", GoodSample = "#00a387"))
+
+# Creates the plot ~
+pheatmap(KMs,
+         border_color="black", annotation = QC, annotation_colors = colours, annotation_legend = TRUE,
          clustering_distance_cols = Dist_Weighted, clustering_distance_rows = "manhattan",
          cellwidth = 10, cellheight = 10,
          cutree_rows = max(5, floor(NR * 20/1000)), cutree_cols = max(5, floor(NC * 20/1000)),
          treeheight_row = max(10, floor(NR * .5)), treeheight_col=max(10,floor(NC * .5)),
-         filename = "FPGP--CovHeatMap.pdf")
-
-str(Oi)
-
-Oi_G <- ggplotGrob(Oi)
-
-print(Oi)
+         filename = "FPG--CovHeatMap.pdf")
 
 
 #
