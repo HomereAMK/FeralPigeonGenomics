026fbd96452e07ce4c7df9a6ce73137df2716246
George Pacheco <37849281+layka-pacheco@users.noreply.github.com>
2021-06-10 15:05:05 +0200

    Minor.

--- FPG--Plots/FPG--Stats/FPG--CovHeatMap/FPGP--CoverageHeatMap.R
+++ FPG--Plots/FPG--Stats/FPG--CovHeatMap/FPGP--CoverageHeatMap.R
@@ -1,57 +1,44 @@
 ### The BEGINNING ~~~~
 ##
-# Creates FPGP--CoverageHeatMap | By George PACHECO
+# ~ Creates FPGP--CoverageHeatMap | By George PACHECO
 
 
 # Sets working directory ~
-
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
-# Loads required packages ~
 
+# Loads required packages ~
 pacman::p_load(pheatmap, tidyverse, ggrepel, extrafont, cowplot)
 
-# Imports extra fonts ~
 
+# Imports extra fonts ~
 loadfonts(device = "win", quiet = TRUE)
 
+
 # Loads data ~
+KMs <- read.table("FPG--Loci_Merged.coverage.km.tsv", sep = "\t", row.names = 1, header = TRUE, check.names = FALSE)
+Weights <- na.omit(as.matrix(read.table("FPG--Loci_Merged.coverage.km_weights.txt", sep = ","))[1, ])
 
-KMs_float16 <- read.table("FPGP--Loci_Merged.coverage_float16.km.tsv", sep = "\t", row.names = 1, header = TRUE, check.names = FALSE)
-Weights_float16 <- na.omit(as.matrix(read.table("FPGP--Loci_Merged.coverage_float16.km_weights.txt", sep = ","))[1, ])
 
-data_weighted_float16 <- KMs * Weights_float16
-dist_weighted_float16 <- dist(t(data_weighted_float16), method = "manhattan")
+# Loads data ~
+KMs_Weighted <- KMs * Weights
+Dist_Weighted <- dist(t(KMs_Weighted), method = "manhattan")
 
-nc_float16 <- ncol(KMs_float16)
-nr_float16 <- nrow(KMs_float16)
 
-KMs_uint8 <- read.table("FPGP--Loci_Merged.coverage_unit8.km.tsv", sep = "\t", row.names = 1, header = TRUE, check.names = FALSE)
-Weights_uint8 <- na.omit(as.matrix(read.table("FPGP--Loci_Merged.coverage_unit8.km_weights.txt", sep = ","))[1, ])
+# Gets number of rows and columns ~
+NC <- ncol(KMs)
+NR <- nrow(KMs)
 
-data_weighted_uint8 <- KMs * Weights_uint8
-dist_weighted_uint8 <- dist(t(data_weighted_uint8), method = "manhattan")
-
-nc_uint8 <- ncol(KMs_uint8)
-nr_uint8 <- nrow(KMs_uint8)
 
 # Creates the plot:
-
-pheatmap(KMs_float16,
+pheatmap(KMs,
          border_color="black",
-         clustering_distance_cols = dist_weighted_float16, clustering_distance_rows = "manhattan",
+         clustering_distance_cols = Dist_Weighted, clustering_distance_rows = "manhattan",
          cellwidth = 10, cellheight = 10,
-         cutree_rows = max(5, floor(nr_float16*20/1000)), cutree_cols = max(5, floor(nc_float16*20/1000)),
-         treeheight_row = max(10, floor(nr_float16*0.5)), treeheight_col=max(10,floor(nc_float16*0.5)),
-         filename = "FPGP--CovHeatMap_float16.pdf")
+         cutree_rows = max(5, floor(NR * 20/1000)), cutree_cols = max(5, floor(NC * 20/1000)),
+         treeheight_row = max(10, floor(NR * .5)), treeheight_col=max(10,floor(NC * .5)),
+         filename = "FPGP--CovHeatMap.pdf")
 
-pheatmap(KMs_uint8,
-         border_color="black",
-         clustering_distance_cols = dist_weighted_uint8, clustering_distance_rows = "manhattan",
-         cellwidth = 10, cellheight = 10,
-         cutree_rows = max(5, floor(nr_uint8*20/1000)), cutree_cols = max(5, floor(nc_uint8*20/1000)),
-         treeheight_row = max(10, floor(nr_uint8*0.5)), treeheight_col=max(10,floor(nc_uint8*0.5)),
-         filename = "FPGP--CovHeatMap_uint8.pdf")
 
 #
 ##
