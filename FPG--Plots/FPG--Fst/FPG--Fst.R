3b49ac3869e8e7c2cb9fbca1225998ad8a705539
George Pacheco <37849281+layka-pacheco@users.noreply.github.com>
2021-06-23 00:14:04 +0200

    Minor.

--- FPG--Plots/FPG--Fst/FPG--Fst.R
+++ FPG--Plots/FPG--Fst/FPG--Fst.R
@@ -12,48 +12,52 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
 
 # Loads required packages ~
-pacman::p_load(pheatmap)
+pacman::p_load(pheatmap, tidyverse, reshape2)
 
 
 # Loads Fst table ~
-data <- read.table("AfterChrGenome_PGP--Fst.tsv", sep = "\t", header = FALSE, stringsAsFactors = FALSE)
+data <- read.table("FPG--Fst.tsv", sep = "\t", header = FALSE, stringsAsFactors = FALSE)
 
 
 # Adds column names ~
 colnames(data) <- c("Pop1", "Pop2", "LociNumber", "Unweighted", "Weighted")
 
-# Removes populations ~
-#data = subset(data, Pop1 != "XXX" & Pop2 != "XXX")
-#data = subset(data, Pop1 != "XXX_1" & Pop2 != "XXX_1")
 
+# Removes populations ~
+data = subset(data, Pop1 != "Cpalumbus" & Pop2 != "Cpalumbus")
+data = subset(data, Pop1 != "TelAvivColony" & Pop2 != "TelAvivColony")
+
+
+# Melts data ~
 pops = union(data$Pop1, data$Pop2)
 n = length(pops)
 
 
-# Removes populations ~
+# Creates Fst-Sites matrix ~
 FstSites <- matrix(0, nrow = n, ncol = n, dimnames = list(pops, pops))
 for (i in 1:nrow(data)) {
-  mat[data[i, "Pop1"], data[i, "Pop2"]] = data[i, "LociNumber"]
-  mat[data[i, "Pop2"], data[i, "Pop1"]] = data[i, "Weighted"]}
+  FstSites[data[i, "Pop1"], data[i, "Pop2"]] = data[i, "LociNumber"]
+  FstSites[data[i, "Pop2"], data[i, "Pop1"]] = data[i, "Weighted"]}
 
 
-# Removes populations ~
-write.table(FstSites, "AfterChrGenome_PGP--Fst_Sites.txt", sep = "\t", row.names = TRUE, col.names = TRUE)
+# Writes Fst-Sites matrix ~
+write.table(FstSites, "FPG--Fst-Sites.txt", sep = "\t", row.names = TRUE, col.names = TRUE)
 
 
-# Removes populations ~
+# Creates Fst-Fst matrix ~
 Fst <- matrix(0, nrow = n, ncol = n, dimnames = list(pops, pops))
-for (i in 1:nrow(T)) {
-  mat[data[i, "Pop1"], data[i, "Pop2"]] = data[i, "Weighted"]
-  mat[data[i, "Pop2"], data[i, "Pop1"]] = data[i, "Weighted"]}
+for (i in 1:nrow(data)) {
+  Fst[data[i, "Pop1"], data[i, "Pop2"]] = data[i, "Weighted"]
+  Fst[data[i, "Pop2"], data[i, "Pop1"]] = data[i, "Weighted"]}
 
 
-# Removes populations ~
-write.table(Fst, "AfterChrGenome_PGP--Fst.txt", sep = "\t", row.names = TRUE, col.names = TRUE)
+# Writes Fst-Fst matrix ~
+write.table(Fst, "FPG--Fst-Fst.txt", sep = "\t", row.names = TRUE, col.names = TRUE)
 
 
-# Removes populations ~
-pheatmap(Fst, treeheight_row = 60, treeheight_col = 60, clustering_distance_rows = "correlation", clustering_distance_cols = "correlation", filename = "FPG--Fst.pdf")
+# Creates & Saves the heatmap ~
+pheatmap(Fst, clustering_distance_rows = "correlation", clustering_distance_cols = "correlation", border_color = "black", cellwidth = 10, cellheight = 10,
+         treeheight_row = 55, treeheight_col = 55, filename = "FPG--Fst.pdf")
 
 
 #
